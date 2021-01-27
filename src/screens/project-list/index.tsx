efc4d5ee174282d25663524495af9fb01820094f
tangxianglin <txline0420@163.com>
2021-01-27 08:34:45 +0800

    [+] js改造成ts

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObject } from "../../utils";
+import qs from "qs";
+
+/*
+    npm start: 读.env.development文件中的REACT_APP_API_URL变量
+    npm build: 读.env文件中的REACT_APP_API_URL变量
+ */
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
